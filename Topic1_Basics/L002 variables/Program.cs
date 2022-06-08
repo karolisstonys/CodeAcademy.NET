09eb7fb22da5a4b8a14c95e203b38706bc9c7bc1
Karolis <4seven@gmail.com>
2022-06-08 14:34:46 +0300

    L002

---
+++ Topic1_Basics/L002 variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L002_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
