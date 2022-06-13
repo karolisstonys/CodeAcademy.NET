cadc60aa352b3f90db5a5934f89e211fbb95e126
Karolis <4seven@gmail.com>
2022-06-13 22:01:12 +0300

    L004 homework

--- Topic1_Basics/L004 tasks/Program.cs
+++ Topic1_Basics/L004 tasks/Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // ====================================================================================================================
             // PARAŠYTI PROGRAMĄ, KURIOJE VARTOTOJO PRAŠOMA ĮVESTI 2 SKAIČIUS.PROGRAMA TURI IŠVESTI
             // - SKAIČIŲ SUMĄ
             // - SKAIČIŲ SKIRTUMĄ
             // - SANDAUGĄ
             // - DALYBĄ
 
+            /*
             decimal num1;
             decimal num2;
 
@@ -23,6 +25,52 @@
             Console.WriteLine($"skaicius1 - skaicius2 = {num1 - num2}");
             Console.WriteLine($"skaicius1 * skaicius2 = {num1 * num2}");
             Console.WriteLine($"skaicius1 / skaicius2 = {num1 / num2}");
+            */
+
+
+
+            // ====================================================================================================================
+            // PARAŠYTI PROGRAMĄ, 3 SKAIČIUS. PROGRAMA TURI IŠVESTI ŠIŲ SKAIČIŲ VIDURKĮ
+
+            decimal numb1;
+            decimal numb2;
+            decimal numb3;
+
+            Console.Write("iveskite skaiciu (1): ");
+            numb1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("iveskite skaiciu (2): ");
+            numb2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("iveskite skaiciu (3): ");
+            numb3 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Skaiciu vidurkis: {(numb1 + numb2 + numb3)/3}");
+
+
+
+            // ====================================================================================================================
+            /*
+            sukurkite naują kintamajį long ir prskirkite didžiausią reikšmę.
+            sukurkite naują kintamajį short ir prskirkite didžiausią reikšmę
+            - padalinkite didesnį skaičių iš mažesnio
+            - iš rezultato atimkite maksimalų long skaičių
+            - ir pridėkite maksimalų int skaičių
+            */
+
+
+
+            // kai dalyba reikia pasicastinti nepamirsti !!! 
+
+
+
+
+
+
+
+
+
+
 
         }
     }
