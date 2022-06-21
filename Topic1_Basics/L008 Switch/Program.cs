e7a422d00c469411fd8afb07d4d8ec8c6c6c360c
Karolis <4seven@gmail.com>
2022-06-21 18:57:08 +0300

    L008 switch

---
+++ Topic1_Basics/L008 Switch/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+Console.Write("Iveskite meniu punkta: ");
+
+int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+switch (menuChoice)
+{
+    case 1:
+        Console.WriteLine("vartotojas pasirinko 1");
+        break;
+    case 2:
+        Console.WriteLine("vartotojas pasirinko 2");
+        break;
+    case 3:
+        Console.WriteLine("vartotojas pasirinko 3");
+        break;
+    default:
+        Console.WriteLine("pasirinkimas negalimas");
+        break;
+}
+
+Console.WriteLine("------------------------------------");
+
+var isvadamasRezultatas = menuChoice switch
+{
+    1 => "vartotojas pasirinko 1",
+    2 => "vartotojas pasirinko 2",
+    3 => "vartotojas pasirinko 3",
+    _ => "vartotojas nieko nepasirinko"
+};
+Console.WriteLine(isvadamasRezultatas);
+
+
+
+// ==================================================================================
+
+
+Console.WriteLine("------------------------------------");
+
+
+switch (menuChoice)
+{
+    case 1:
+    case 2:
+        Console.WriteLine("vartotojas pasirinko 1 arba 2");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
