c30315183f41597d96c99264c00aab0681afb640
Karolis <4seven@gmail.com>
2022-07-13 18:22:37 +0300

    L018

---
+++ Topic1_Basics/L018_Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace L018_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            NAUJAS
+            ----------------------------
+            Parašykite metodą SkaiciuEile kuri išvestu vienoje eilutėje skaičių grupes tokiu principu: -> 1 -> 11 -> 111 -> 1111 -> 11111 -> .......
+            programa turi paprašyti nurodyti skaičių ir grupių kiekį
+            naudokite for ir StringBuilder
+            */
+
+
+
+
+
+        }
+    }
+}
