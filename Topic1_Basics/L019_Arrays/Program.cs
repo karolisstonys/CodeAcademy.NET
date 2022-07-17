a92c48eba803d0b54b84006086631abf22319b87
Karolis <4seven@gmail.com>
2022-07-17 14:15:16 +0300

    namu darbai

--- Topic1_Basics/L019_Arrays/Program.cs
+++ Topic1_Basics/L019_Arrays/Program.cs
@@ -15,7 +15,7 @@ namespace L019_Arrays
             ///     0101
             ///     10101
             /// </summary>
-            PirmaUzduotis();
+            //PirmaUzduotis();
 
             /// <summary>
             /// 2. Parasykite programa, kuri paprasytu ivesti skaiciu ir ivesta skaiciu atspausdintu atvirkstine seka. 
@@ -25,6 +25,65 @@ namespace L019_Arrays
             ///     Rezultatas-54321 (int)
             /// </summary>
             //AntraUzduotis();
+
+            /// <summary>
+            /// 3. Parasykite programa, kuri leistu ivesti kiek zmoniu siandiena atejo i pamoka. 
+            /// Ivedus skaiciu programa prasytu ivesti visu atejusiu zmoniu vardus. 
+            /// Kada visi vardai buna ivesti programa turetu atspausdinti visu vardus atvirkstine seka.
+            ///     Pvz:
+            ///     3
+            ///     Edvinas
+            ///     Jonas
+            ///     Petras
+            ///     ----------
+            ///     Petras
+            ///     Jonas
+            ///     Edvinas
+            /// </summary>
+
+
+            /// <summary>
+            /// 4. Parasykite programa, kuri leistu ivesti kiek zmoniu siandiena atejo i pamoka. 
+            /// Ivedus skaiciu programa prasytu ivesti visu atejusiu zmoniu vardus. 
+            /// Kada visi vardai buna ivesti programa turetu atspausdinti ilgiausia varda ekrane. 
+            /// Jei vardai yra vienodo ilgio turetu atspausdinti abu vardus.
+            ///     Pvz:             
+            ///     3            
+            ///     Edvinas            
+            ///     Jonas            
+            ///     Petras
+            ///     ---------------------            
+            ///     Edvinas
+            /// </summary>
+
+
+            /// <summary>
+            /// 5. Parasykite programa, kuri rastu visus pasikartojancius skaicius duotame masyve ir juos atvaizduotu ekrane
+            ///     PVZ: 1,2,2,4,2,7,6,1         
+            ///     Pasikartojantys skaiciai: 1, 2
+            /// </summary>
+
+
+            /// <summary>
+            /// 6. Programa praso ivesti eiluciu ir stulpeliu kieki. 
+            /// Ivedus turetu sukurti masyva duoto dydzio, paprasyti ivesti kiekvieno elemento skaiciu/reiksme ir 
+            /// atspausdintu matrica is pateiktu skaiciu            
+            /// PVZ: Ivedame 2 2. Suvedame 1, 2, 2, 3                 
+            ///     1 2                 
+            ///     2 3
+            /// </summary>
+            /// 
+
+
+            /// <summary>
+            /// 7.Parasykite programa, kuri rastu visus pasikartojancius skaicius duotame dvimaciame masyve ir juos atvaizduotu ekrane
+            /// </summary>
+            /// 
+
+
+            /// <summary>
+            /// 8. Parasykite programa, kuri rastu visus pasikartojancius vardus duotame dvimaciame masyve ir juos atvaizduotu ekrane
+            /// </summary>
 
 
         }
