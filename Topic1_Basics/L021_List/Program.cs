17f3bec4821aada2afb412b4c55b4ec3fe7d66dd
Karolis <4seven@gmail.com>
2022-07-19 20:12:49 +0300

    Push

---
+++ Topic1_Basics/L021_List/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace L021_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+        }
+
+
+        private static void EXAMPLES()
+        {
+            List<string> stringSarasas = new List<string> { "zodis1", "zodis2", "........" };
+
+            List<int> intSarasas = new List<int> { 1, 22, 33, 44, 5 };
+
+            List<string> automobiliai = new List<string> { "Audi", "VW", "Opel", "Volvo" };
+            Console.WriteLine(string.Join(", ", automobiliai));
+
+
+            Console.WriteLine(string.Join(", ", automobiliai));
+
+            automobiliai[1] = "Subaru";
+            Console.WriteLine(string.Join(", ", automobiliai));
+            Console.WriteLine("  "+ automobiliai[2][1]);
+            Console.WriteLine("  "+ automobiliai[2]);
+            Console.WriteLine("  " + automobiliai[2].Length);
+
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+            Console.WriteLine("Listo talpa " + automobiliai.Capacity);
+
+            automobiliai.Add("BMW");
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+            Console.WriteLine("Listo talpa " + automobiliai.Capacity);
+
+            //Metodai
+            //prideti elementa i gala
+            automobiliai.Add("Citroen");
+            Console.WriteLine(string.Join(", ", automobiliai));
+
+            //prideti daug elementu i gala
+            List<string> daugiauAutomobiliu = new List<string> { "BMW", "Mercedes", "Toyota" };
+            automobiliai.AddRange(daugiauAutomobiliu);
+            Console.WriteLine(string.Join(", ", automobiliai));
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+            Console.WriteLine("automobilis nr 5  " + automobiliai[5]);
+
+            //isvalyti lista
+            automobiliai.Clear();
+            Console.WriteLine("isvalyta " + string.Join(", ", automobiliai));
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+
+            //istrinti elementa
+            automobiliai.RemoveAt(4);
+            Console.WriteLine(string.Join(", ", automobiliai));
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+            Console.WriteLine("automobilis nr 5  " + automobiliai[5]);
+
+            //iterpimas
+            Console.WriteLine("-------------------------");
+            automobiliai.Insert(2, "BMW");
+            Console.WriteLine(string.Join(", ", automobiliai));
+            Console.WriteLine("automobiliu kiekis " + automobiliai.Count);
+            Console.WriteLine("automobilis nr 5  " + automobiliai[5]);
+
+            //paieska
+            Console.WriteLine("-------------------------");
+            bool arYraVw = automobiliai.Contains("VW");
+            Console.WriteLine("Ar yra VW? " + arYraVw);
+            bool arYraBmw = automobiliai.Contains("BMW");
+            Console.WriteLine("Ar yra BMW? " + arYraBmw);
+
+            //rikiavimas
+            Console.WriteLine("-------------------------");
+            automobiliai.Sort();
+            Console.WriteLine(string.Join(", ", automobiliai));
+
+            automobiliai.Sort((x, y) => y.CompareTo(x));
+            Console.WriteLine(string.Join(", ", automobiliai));
+
+
+            Console.WriteLine("-------------------------");
+            intSarasas.Sort();
+            Console.WriteLine(string.Join(", ", intSarasas));
+            intSarasas.Sort((x, y) => y - x);
+            Console.WriteLine(string.Join(", ", intSarasas));
+
+            string[] automobiliuMasyvas = automobiliai.ToArray();
+
+            int[] intMasyvas = new int[] { 1, 2, 3, 4, 5 };
+            List<int> skaiciai = intMasyvas.ToList();
+        }
+
+    }
+}
