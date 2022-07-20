4273b760dedee2569c4aad872c7296b8a3745b9e
Karolis <4seven@gmail.com>
2022-07-20 22:06:22 +0300

    push

--- Topic1_Basics/L022_ForEach/Program.cs
+++ Topic1_Basics/L022_ForEach/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace L022_ForEach
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Labas as esu Kodelskis ir labai megstu programuoti"); 
+            Console.WriteLine(Task5Split("Labas as esu Kodelskis ir labai megstu programuoti"));
         }
 
         private static void EXAMPLES()
@@ -45,19 +46,99 @@
             }
         }
 
+        #region KLASES DARBAS 1. Parasykite programa, kuri apskaiciuotu duoto integer saraso vidurki
+        public static double Task1Average(List<int> li)
+        {
+            double average = 0;
+            foreach (var num in li)
+            {
+                average += num;
+            }
+            return average / li.Count;
+        }
+        #endregion
 
+        #region KLASES DARBAS 2. Parasykite metoda, kuris grazina ar skaicius neigiamas ar teigiamas masyve.
+        public static List<string> Task2CheckIfPositive(List<int> li)
+        {
+            List<string> positivity = new List<string>();
+            foreach (var num in li)
+            {
+                if (num >= 0)
+                    positivity.Add("teigiamas");
+                else
+                    positivity.Add("neigiamas");
+            }
+            return positivity;
+        }
+        #endregion
 
+        #region KLASES DARBAS 3. Parasykite metoda, kuris apskaiciuos kiek jums reikes sumoketi GPM nuo duoto imoku saraso. Sio uzdavinio GPM: 15%
+        public static double Task3Tax(List<double> li)
+        {
+            double tax = 0;
+            foreach (var item in li)
+            {
+                tax += item;
+            }
+            return tax * 0.15d;
+        }
+        #endregion
 
+        /* 4. Parašyti metodą IstrauktiSkaicius, kuris priima teksta, bet grazina tik skaicius egzistuojancius paciame tekste.
+        Isgavus teksta programa turetu naudoti naujai sukurta SurikiuotiSkaiciusIsTeksto metoda, kuris priima "string skaiciaiTekste" ir surikiuoja skaicius
+        didejimo tvarka. SurikiuotiSkaiciusIsTeksto privalo panaudoti foreach, kad suformuotumet nauja List<int>:
+        PVZ: Ivedame: 1sd512sd5. Programa be rusiavimo grazina mums: 15125. Programa su rusiavimu grazina mums: 11255 */
+        public static string Task4ExtractNumbers(string text)
+        {
+            string onlyNumbers = "";
+            foreach (char character in text)
+            {
+                if (character >= 48 && character <= 57)
+                    onlyNumbers += character;
+            }
+            return onlyNumbers;
+        }
 
+        public static string Task4Ascending(string text)
+        {
+            List<int> digits = new List<int>();
+            foreach (char character in text)
+            {
+                digits.Add(character - 48);
+            }
 
+            for (int i = 0; i < digits.Count; i++)
+            {
+                for (int j = i + 1; j < digits.Count; j++)
+                {
+                    if (digits[j] < digits[i])
+                    { 
+                        int tempI = digits[i];
+                        digits[i] = digits[j];
+                        digits[j] = tempI;
+                    }
+                }
+            }
+            return String.Join("", digits);
+        }
 
+        /* 5. Parašyti metodą IsmetytiZodzius, kuris priima sakini, bet grazina nauja zodziu List sudaryta tik is zodziu, 
+        kurie ilgesni ARBA LYGUS nei 5 raides ir yra surikiuoti abeceles tvarka.
+        Tada parasykite metoda, kuris priima 2 zodziu sarasus, juos sujungia i viena kolekcija naudojant ciklus ir atspausdina ekrane.
+        
+        PRIMINIMAS: Kad isskirti string i atskirus zodzius naudokite pavyzdinis String.Split(' ')        
+                                                                                      .sort()
+        
+                             PVZ: Ivedame: "Labas as esu Kodelskis ir labai megstu programuoti".
+        Programa be rusiavimo grazina mums: as esu ir Labas Kodelskis labai megstu programuoti
+        Programa su rusiavimu grazina mums: as esu ir Kodelskis labai Labas megstu programuoti */
 
+        public static string Task5Split(string text)
+        {
 
-
-
-
-
-
+            return "";
+        }
 
 
 
---
+++ Topic1_Basics/L022_ForEach_Tests/ProgramTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using L022_ForEach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L022_ForEach.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Task1Average_Test()
+        {
+            var fake = new List<int> { 5, 1, 6, 8, 7, 9 };
+            double expected = 6;
+            double actual = L022_ForEach.Program.Task1Average(fake);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task2CheckIfPositive_Test()
+        {
+            var fake = new List<int> { -5, 0, -6, -8, 7, 9 };
+            List<string> expected = new List<string> { "neigiamas", "teigiamas", "neigiamas", "neigiamas", "teigiamas", "teigiamas" };
+            List<string> actual = L022_ForEach.Program.Task2CheckIfPositive(fake);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task3Tax_Test()
+        {
+            var fake = new List<double> { 500, 112, 660, 880, 770, 5, 910 };
+            double expected = 575.55;
+            double actual = L022_ForEach.Program.Task3Tax(fake);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task4ExtractNumbers_Test()
+        {
+            var fake = "1sd512sd5";
+            var expected = "15125";
+            var actual = L022_ForEach.Program.Task4ExtractNumbers(fake);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task4Ascending_Test()
+        {
+            var fake = "15125";
+            var expected = "11255";
+            var actual = L022_ForEach.Program.Task4Ascending(fake);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
