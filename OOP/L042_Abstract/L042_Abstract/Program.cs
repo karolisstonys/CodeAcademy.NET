15d5ec6da64f16abbb7a79ad7ba1dad78d2422ae
Karolis <4seven@gmail.com>
2022-08-29 18:22:04 +0300

    L042

---
+++ OOP/L042_Abstract/L042_Abstract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L042_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
