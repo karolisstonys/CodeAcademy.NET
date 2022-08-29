a725f303f00d989c5851234a649d3ce0e2f56a6a
Karolis <4seven@gmail.com>
2022-08-29 18:42:37 +0300

    abstract class

--- OOP/L042_Abstract/L042_Abstract/Program.cs
+++ OOP/L042_Abstract/L042_Abstract/Program.cs
@@ -5,6 +5,35 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // Abstract class negali sukurti naujo instance
+            //SearchEngine searchEngine = new SearchEngine();
+
+            SearchEngine googleSearchEngine = new GoogleSearchEngine();
+            SearchEngine duckDuckGoSearchEngine = new DuckDuckGoSearchEngine();
+
+            string[] googleResults = googleSearchEngine.Search("aaa");
+            string[] duckDuckGoResults = duckDuckGoSearchEngine.Search("bbb");
+            Console.WriteLine($"google results: [{string.Join(", ", googleResults)}]");
+            Console.WriteLine($"duckduckgo results: [{string.Join(", ", duckDuckGoResults)}]");
+
+
+
+            string[] googleResults1 = googleSearchEngine.Result();
+            string[] duckDuckGoResults1 = duckDuckGoSearchEngine.Result();
+            Console.WriteLine($"google results: [{string.Join(", ", googleResults1)}]");
+            Console.WriteLine($"duckduckgo results: [{string.Join(", ", duckDuckGoResults1)}]");
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
--- OOP/L042_Abstract/L042_Abstract/SearchEngine.cs
+++ OOP/L042_Abstract/L042_Abstract/SearchEngine.cs
@@ -6,8 +6,64 @@ using System.Threading.Tasks;
 
 namespace L042_Abstract
 {
-    internal class SearchEngine
+    public abstract class SearchEngine
     {
+        public abstract string[] Search(string text);
 
+        //public virtual string[]? Search(string text)
+        //{
+        //    return null;
+        //}
+
+
+        public virtual string[] Result()
+        {
+            return new string[]
+            {
+                "NO RESULT"
+            };
+        }
     }
+
+    public class GoogleSearchEngine : SearchEngine
+    {
+        public override string[] Search(string text)
+        {
+            return new string[]
+            {
+                "Google result 1",
+                "Google result 2",
+                "Google result 3",
+                "Google result 4",
+                "Google result 5"
+            };
+        }
+    }
+    public class DuckDuckGoSearchEngine : SearchEngine
+    {
+        public override string[] Search(string text)
+        {
+            return new string[]
+            {
+                "DuckDuckGo result 1",
+                "DuckDuckGo result 2",
+                "DuckDuckGo result 3",
+                "DuckDuckGo result 4",
+                "DuckDuckGo result 5"
+            };
+        }
+
+        public override string[] Result()
+        {
+            return new string[]
+            {
+                "DuckDuckGo result 1",
+                "DuckDuckGo result 2",
+                "DuckDuckGo result 3",
+                "DuckDuckGo result 4",
+                "DuckDuckGo result 5"
+            };
+        }
+    }
+
 }
