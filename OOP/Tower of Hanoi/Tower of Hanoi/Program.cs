15b85da4361f08215b391e5506d7fac35f926e4a
Karolis <4seven@gmail.com>
2022-09-05 19:09:29 +0300

    Tower of Hanoi

---
+++ OOP/Tower of Hanoi/Tower of Hanoi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tower_of_Hanoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Tower of Hanoi!");
+        }
+    }
+}
