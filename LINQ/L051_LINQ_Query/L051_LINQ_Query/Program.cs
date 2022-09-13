76abdef83698ef53f558f394f8ce136343cb3f5d
Karolis <4seven@gmail.com>
2022-09-13 22:00:09 +0300

    push

--- LINQ/L051_LINQ_Query/L051_LINQ_Query/Program.cs
+++ LINQ/L051_LINQ_Query/L051_LINQ_Query/Program.cs
@@ -15,8 +15,8 @@ namespace L051_LINQ_Query
                 new Person { Id = 1, Name = "Aluizas", Age = 22, ProfessionId = 1 },
                 new Person { Id = 1, Name = "Antanas", Age = 19, ProfessionId = 4 },
                 new Person { Id = 1, Name = "Daiva", Age = 26, ProfessionId = 2 },
-                new Person { Id = 1, Name = "Rasa", Age = 30, ProfessionId = 3 },
-                new Person { Id = 1, Name = "Agne", Age = 26, ProfessionId = 1 },
+                new Person { Id = 1, Name = "Rasa", Age = 33, ProfessionId = 3 },
+                new Person { Id = 1, Name = "Agne", Age = 18, ProfessionId = 1 },
             };
 
             List<Profession> profesions = new List<Profession>
@@ -36,7 +36,59 @@ namespace L051_LINQ_Query
             };
 
 
+            // filtruojame studentus kuriems yra maziau nei 20 metu
+            var jauniStudentai = from s in students
+                                 where s.Age < 20
+                                 select s;
+
+
+
+            var surikiuotiStudentai = from s in students
+                                      orderby s.Age
+                                      select s;
+
+
+
+            var evenNumbersFromList = Task1(new List<int> { 0,1,2,3,4,5,6,7,8,9 });
+            
+            foreach (var number in evenNumbersFromList)
+            {
+                Console.Write(number + " ");
+            }
+
 
         }
+
+        public static IEnumerable<int> Task1(List<int> list)
+        {
+             var evenNumbersFromList = from l in list
+                                      where l % 2 == 0
+                                      select l;
+            return evenNumbersFromList;
+        }
+
+
+        public static List<int> Task2(List<int> list)
+        {
+            var possitiveNumbersFromList = from l in list
+                                      where l > 0
+                                      select l;
+            return possitiveNumbersFromList.ToList();
+        }
+
+        public static List<int> Task3(List<int> list)
+        {
+            var possitiveNumbersFromList = from l in list
+                                           select l * l;
+            return possitiveNumbersFromList.ToList();
+        }
+
+        public static List<int> Task4(List<int> list)
+        {
+            var possitiveNumbersFromList = from l in list
+                                           select l;
+            return possitiveNumbersFromList.ToList();
+        }
+
     }
 }
---
+++ LINQ/L051_LINQ_Query/L051_LINQ_Query_Tests/Program_Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using L051_LINQ_Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L051_LINQ_Query_Tests
+{
+    [TestClass()]
+    public class Program_Tests
+    {
+        [TestMethod()]
+        public void Task2_Test()
+        {
+            //Arrange
+            var list = new List<int> { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var expected = new List<int> { 1, 3, 12, 19, 6, 9, 10, 14 };
+            //Act
+            var actual = Program.Task2(list);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task3_Test()
+        {
+            //Arrange
+            var list = new List<int> { 3, 9, 2, 8, 6, 5 };
+            var expected = new List<int> { 9, 81, 4, 64, 36, 25 };
+            //Act
+            var actual = Program.Task3(list);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
