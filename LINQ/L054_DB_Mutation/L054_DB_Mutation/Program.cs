e59fe307abe56fb2b306d688b005a8a0a9104132
Karolis <4seven@gmail.com>
2022-09-19 18:32:26 +0300

    DB Mutation

---
+++ LINQ/L054_DB_Mutation/L054_DB_Mutation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L054_DB_Mutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
