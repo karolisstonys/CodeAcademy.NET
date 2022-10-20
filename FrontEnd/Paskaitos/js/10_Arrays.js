d9417528ca9ab8abcd43e0de51850a66f14a743b
Karolis <4seven@gmail.com>
2022-10-20 20:17:19 +0300

    js functions

--- FrontEnd/Paskaitos/js/10_Arrays.js
+++ FrontEnd/Paskaitos/js/10_Arrays.js
@@ -95,6 +95,214 @@ let mathPi = Math.PI;
 let mathValueRound = Math.round(12.698);
 
 
+function dice() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log(`Die was rolled: ${roll}`);
+}
+
+
+
+function throwDice() {
+    dice();
+}
+
+function throwDice(count) {
+    for (let i = 0; i < count; i++) {
+        dice();
+    }
+}
+
+
+
+// FUNCTIONS WITH ARGUMENTS
+console.log('hello'.toUpperCase());
+console.log('hello'.indexOf('l'));
+
+function greet(name) {
+    console.log(`Hello ${name}!`);
+    console.log(`Nice to meet you!`);
+}
+
+greet();
+greet(null);
+greet('Edvinas');
+
+function throwDice(numRolls) {
+    for (let i = 0; i < numRolls; i++) {
+        throwDie();
+    }
+}
+
+throwDice(5); // Plain JS does not support overloading
+
+function square(num) {
+    console.log(num * num);
+}
+
+square(9);
+
+function sum(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+sum(8, 9);
+
+function divide(firstNumber, secondNumber) {
+    console.log(firstNumber / secondNumber);
+}
+
+divide(10, 3);
+
+
+
+
+// FUNCTIONS RETURNING VALUES 
+const yell = "welcome".toUpperCase();
+console.log(yell);
+
+function sumReturn(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+console.log(sumReturn(87, 56));
+
+function sumBrokenReturn(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+    console.log(firstNumber + secondNumber); // Neigyvendina sios dalies, nes jau grazinta reiksme
+}
+
+function sumTextReturn(firstNumber, secondNumber) {
+    return `Sum: ${firstNumber + secondNumber}`;
+}
+
+console.log(sumTextReturn(78, 89));
+
+function isGreen(color) {
+    if (color.toLowerCase() === 'green') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(`isGreen: ${isGreen('Green')}`);
+console.log(`isGreen: ${isGreen('Blue')}`);
+
+function isBetterGreen(color) {
+    if (color.toLowerCase() === 'green') {
+        return true;
+    }
+
+    return false;
+}
+
+function isBestGreen(color) {
+    return color.toLowerCase() === 'green';
+}
+
+function containGreen(arr) {
+    for (let color of arr) {
+        if (color.toLowerCase() === 'green' || color.toLowerCase() === 'lime') {
+            return true;
+        }
+        // return false;
+    }
+
+    return false;
+}
+
+let colorArr = ['Pink', 'Blue', 'Green'];
+
+console.log(`Contains green: ${containGreen(colorArr)}`);
+
+
+
+
+
+// SCOPE
+// Function scope
+function scopeExample() {
+    let scopeName = "Example scope";
+    const creationDate = Date.UTC();
+    var tag = "Learning";
+    console.log(tag);
+}
+
+// console.log(tag); // Not possible to access direct function scope
+
+scopeExample();
+
+
+
+// Block scope
+let animal1 = 'Dog';
+
+function PrintAnimal1() { // Function scope opens
+    let animal1 = 'Cat';
+    console.log(animal1);
+
+    if (true) { // Block scope opens
+        var animal2 = "Owl"; // var is scoped only to functions
+        console.log(animal2);
+    }
+}
+
+PrintAnimal1();
+console.log(animal1);
+
+
+
+if (true) { // Block scope opens
+    var animal2 = "Owl"; // var is scoped only to functions
+    console.log(animal2);
+}
+
+console.log(`Outer animal2 call: ${animal2}`);
+
+var animals = ["Bear", "Deer", "Zebra"];
+
+var i = 10;
+for (var i = 0; i < animals.length; i++) { // Block scope opens
+    console.log(i, animals[i]);
+}
+
+console.log(i); // With let - 10 if with var - 3
+
+
+function doubleEleInArray(arr) {
+    const result = [];
+    for (let num of arr) {
+        let double = num * 2;
+        result[result.length] = double;
+    }
+
+    return result;
+}
+
+let doubleNumbersArr = [5, 8, 9, 7, 4, 7];
+console.log(doubleEleInArray(doubleNumbersArr));
+
+
+
+// Lexical scope
+function outer() {
+    let movie = 'Amadeus';
+
+    function inner() {
+        console.log(movie.toUpperCase());
+
+        function extraInner() {
+            console.log(movie.toUpperCase());
+        }
+
+        extraInner();
+    }
+
+    inner();
+}
+
+outer();
 
 
 
@@ -102,6 +310,109 @@ let mathValueRound = Math.round(12.698);
 
 
 
+// FUNCTION EXPRESSIONS
+// Functions are objects
+console.dir(sum);
+
+// Annonymous
+const sumAnnon = function (a, b) {
+    return a + b;
+}
+
+console.log(sumAnnon(5, 5));
+
+
+// Named
+const product = function multiplyNamed(a, b) {
+    return a * b;
+}
+
+console.log(product(5, 5));
+
+
+
+
+// HIGHER ORDER FUNCTIONS
+// Tai yra funkcijos, kurios priima funkcijas kaip parametrus arba gražina funkcijas kaip return value
+
+
+function addFtion(a, b) {
+    return a + b;
+}
+
+const substractFtion = function (a, b) {
+    return a - b;
+}
+
+function multiplyFtion(a, b) {
+    return a * b;
+}
+
+const divideFtion = function (a, b) {
+    return a / b;
+}
+
+const operations = [addFtion, substractFtion, multiplyFtion, divideFtion];
+
+// console.log(operations[0]);
+// console.log(operations[0](100,4));
+
+for (let func of operations) {
+    let result = func(50, 8);
+    console.log(result);
+}
+
+// Class in JS gets added with a function
+const thing = {
+    //aaa: 'asdas',
+    doSomething: multiplyFtion
+}
+
+console.log(thing.doSomething(70, 8));
+
+console.log("asd".toUpperCase());
+console.log("asd".indexOf("d"));
+
+// FUNCTION AS PARAMETER
+function callThreeTimes(func) {
+    func();
+    func();
+    func();
+}
+
+function callGeneric() {
+    console.log("Generic call");
+}
+
+function callMoreGeneric() {
+    console.log("More generic call");
+}
+
+callThreeTimes(callGeneric);
+callThreeTimes(callMoreGeneric);
+
+// Funkcijos deklaravimas
+function repeatNTimes(action, num) {
+    for (let i = 0; i < num; i++) {
+        action();
+    }
+}
+
+repeatNTimes(callGeneric, 5);
+
+
+function pickOne(f1, f2) {
+    let rand = Math.random();
+    console.log(rand);
+    if (rand < 0.5) {
+        f1();
+    }
+    else {
+        f2();
+    }
+}
+
+pickOne(callGeneric, callMoreGeneric);
 
 
 
