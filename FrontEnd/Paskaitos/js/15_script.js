f40e7f3cc84deff392d87401b74459814e966290
Karolis <4seven@gmail.com>
2022-10-31 19:45:32 +0200

    sessionStorage & LocalStorage

---
+++ FrontEnd/Paskaitos/js/15_script.js
@@ -0,0 +1,71 @@
+
+// ********************* GET *********************
+const showValues = () => {
+    lbl_mokymai_key.innerHTML = sessionStorage.mokymai_key ?? '-';
+    lbl_kitas_raktas.innerHTML = sessionStorage.kitas_raktas ?? '-';
+    lbl_mokymai_localStorage.innerHTML = localStorage.mokymai_localStorage ?? '-';
+    lbl_json_localStorage.innerHTML = localStorage.json_localStorage ?? '-';
+}
+document.addEventListener('DOMContentLoaded', showValues);
+
+
+
+// ********************* SET *********************
+
+btn_setSessionStorage.addEventListener('click', () => {
+    sessionStorage.setItem('mokymai_key', 'kazkokia nesudetinga reiksme');
+    txt_setSessionStorage.innerHTML = 'irasyta i session storage sekmingai';
+    txt_setSessionStorage.style.color = 'green';
+})
+
+btn_setSessionStorage1.addEventListener('click', () => {
+    sessionStorage.setItem('kitas_raktas', 'kazkokia kito rakto reiksme');
+    txt_setSessionStorage1.innerHTML = 'irasyta i session storage sekmingai';
+    txt_setSessionStorage1.style.color = 'green';
+})
+
+btn_setLocalStorage.addEventListener('click', () => {
+    const asmuo = {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis'
+    }
+    localStorage.setItem('mokymai_localStorage', asmuo);
+    txt_setLocalStorage.innerHTML = 'irasyta i local storage sekmingai';
+    txt_setLocalStorage.style.color = 'blue';
+})
+
+btn_json_setLocalStorage.addEventListener('click', () => {
+    const asmuo = {
+        vardas: 'Vardenis',
+        pavarde: 'Pavardenis'
+    }
+    localStorage.setItem('json_localStorage', JSON.stringify(asmuo));
+    txt_json_setLocalStorage.innerHTML = 'irasyta i local storage sekmingai';
+    txt_json_setLocalStorage.style.color = 'blue';
+})
+
+// ********************* GET *********************
+
+btn_getMokymaiKey.addEventListener('click', () => {
+    const val = sessionStorage.getItem('mokymai_key');
+    txt_getMokymaiKey.innerHTML = val ?? 'nieko nera';
+})
+btn_get_kitas_raktas.addEventListener('click', () => {
+    const val = sessionStorage.getItem('kitas_raktas');
+    txt_get_kitas_raktas.innerHTML = val ?? 'nieko nera';
+})
+btn_get_mokymai_localStorage.addEventListener('click', () => {
+    const val = localStorage.mokymai_localStorage;  // Object-like access
+    txt_get_mokymai_localStorage.innerHTML = val ?? 'nieko nera';
+})
+btn_getLocalStorage.onclick = () => {               // .onclick atributas
+    const val = localStorage.json_localStorage;     // Object-like access
+    txt_getLocalStorage.innerHTML = val ?? 'nieko nera';
+};
+
+
+
+
+
+
+
