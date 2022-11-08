d5cacb0562d782e694c2e06ff3b11b07861ba748
Karolis <4seven@gmail.com>
2022-11-08 21:26:08 +0200

    js fetch api tasks done

--- FrontEnd/Paskaitos/js/17_fetch_api.js
+++ FrontEnd/Paskaitos/js/17_fetch_api.js
@@ -1,8 +1,8 @@
-//const url = 'https://swapi.dev/api/starships';
-//const options = {};
+const urlStarships = 'https://swapi.dev/api/starships';
+const options = '';
 
-/*const getSwapiStarship = (obj) => {
-    fetch(url, options)
+const getSwapiStarship = (obj) => {
+    fetch(urlStarships, options)
         .then(response => response.json())
         .then(data => {
             starships_name.innerHTML = data.results[obj].name;
@@ -16,7 +16,7 @@
             starships_cargo_capacity.innerHTML = "cargo capacity: " + data.results[obj].cargo_capacity;
             starships_consumables.innerHTML = "consumables: " + data.results[obj].consumables;
         });
-}*/
+}
 
 // ------------------------------------------------------------------------------------------------------------
 
@@ -45,29 +45,19 @@ const loadDataAsync = async () => {
 
 // ------------------------------------------------------------------------------------------------------------
 
-const urlStarships = 'https://swapi.dev/api/starships';
-const getSwapiStarship = async (obj) => {
-    const response = await fetch(urlStarships);
-    const data = await response.json();
-    starships_name.innerHTML = data.results[obj].name;
-    starships_model.innerHTML = "model: " + data.results[obj].model;
-    starships_manufacturer.innerHTML = "manufacturer: " + data.results[obj].manufacturer;
-    starships_cost_in_credits.innerHTML = "cost in credits: " + data.results[obj].cost_in_credits;
-    starships_length.innerHTML = "length: " + data.results[obj].length;
-    starships_max_atmosphering_speed.innerHTML = "max atmosphering speed: " + data.results[obj].max_atmosphering_speed;
-    starships_crew.innerHTML = "crew: " + data.results[obj].crew;
-    starships_passengers.innerHTML = "passengers: " + data.results[obj].passengers;
-    starships_cargo_capacity.innerHTML = "cargo capacity: " + data.results[obj].cargo_capacity;
-    starships_consumables.innerHTML = "consumables: " + data.results[obj].consumables;
-    console.log(data);
-}
-
 const urlPlanets = 'https://swapi.dev/api/planets';
 
 const getSwapiAllPlanetsCount = async () => {
+    const response = await fetch(urlPlanets);
+    const data = await response.json();
 
+    for (let i = 0; i < data.results.length; i++) {
+        // fake fetch await just to show delay
+        const response2 = await fetch(urlPlanets);
+        const data2 = await response2.json();
+        div_swapi_planet_counter.innerHTML += `<div id="planet_index_${i}"><div>`;
+    }
 }
-
 
 const getSwapiAllPlanets = async () => {
     const response = await fetch(urlPlanets);
@@ -78,7 +68,45 @@ const getSwapiAllPlanets = async () => {
         // fake fetch await just to show delay
         const response2 = await fetch(urlPlanets);
         const data2 = await response2.json();
-        table_swapi_planets.innerHTML += `<tr><td>${data2.results[i].name}</td><td>${data2.results[i].diameter}</td><td>${data2.results[i].climate}</td><td>${data2.results[i].gravity}</td><td>${data2.results[i].terrain}</td><td>${data2.results[i].population}</td><td>${data2.results[i].residents.length}</td><td>${data2.results[i].films.length}</td></td>`;
+        table_swapi_planets.innerHTML += `<tr><td>${data2.results[i].name}</td><td>${data2.results[i].diameter}</td><td>${data2.results[i].climate}</td><td>${data2.results[i].gravity}</td><td>${data2.results[i].terrain}</td><td>${data2.results[i].population}</td><td id="td_${i}_residents">${data2.results[i].residents.length}</td><td id="td_${i}_films">${data2.results[i].films.length}</td></td>`;
+        document.getElementById('planet_index_' + i).style.backgroundColor = "black";
+    }
+}
+
+const getSwapiAllResidents = async () => {
+    const response = await fetch(urlPlanets);
+    const data = await response.json();
+
+    for (let i = 0; i < data.results.length; i++) {
+        let td = document.getElementById('td_' + i + '_residents');
+        td.innerHTML = '';
+
+        for (const resident of data.results[i].residents) {
+            const residentResponse = await fetch(resident);
+            const residentData = await residentResponse.json();
+            if (residentData.gender === "male")
+                td.innerHTML += `<span title="${residentData.name}">👨</span>`;
+            else if (residentData.gender === "female")
+                td.innerHTML += `<span title="${residentData.name}">👩</span>`;
+            else
+                td.innerHTML += `<span title="${residentData.name}">👽</span>`;
+        }
+    }
+}
+
+const getSwapiAllFilms = async () => {
+    const response = await fetch(urlPlanets);
+    const data = await response.json();
+
+    for (let i = 0; i < data.results.length; i++) {
+        let td = document.getElementById('td_' + i + '_films');
+        td.innerHTML = '';
+
+        for (const film of data.results[i].films) {
+            const filmResponse = await fetch(film);
+            const filmData = await filmResponse.json();
+            td.innerHTML += `<span title="${filmData.title}">🎬</span>`;
+        }
     }
 }
 
