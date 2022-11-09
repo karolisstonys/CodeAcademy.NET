e67fe22e28e2ee81217d0cfb3f72ef5fd4578da7
Karolis <4seven@gmail.com>
2022-11-10 00:05:15 +0200

    front end final project in progress

---
+++ FrontEnd/Front_End_Final_Project/js/create_user.js
@@ -0,0 +1,46 @@
+const goToCreateUser = () => {
+    window.location.href = "index.html";
+}
+
+p_login.addEventListener('click', goToCreateUser);
+
+const message = p_message;
+const form_create_user = document.querySelector('#form_create_user');
+
+function createNewUser() {
+    let form = new FormData(form_create_user);
+    let newObject = {};
+
+    console.log(form);
+
+    form.forEach((value, key) => { newObject[key] = value });
+
+    fetch('https://testapi.io/api/4seven/resource/Users', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newObject)
+    })
+        .then(obj => {
+            console.log(obj.json())
+            message.innerHTML = 'sukurta rasta!';
+        })
+        .catch((err) => message.innerHTML = err);
+}
+
+const form_create_user_submit = document.querySelector('#form_create_user_submit');
+form_create_user_submit.addEventListener('click', (e) => {
+    e.preventDefault(); // Breaks manual refresh after submit
+    createNewUser();
+})
+
+
+
+
+
+
+
+
+
--- FrontEnd/Front_End_Final_Project/js/index.js
+++ FrontEnd/Front_End_Final_Project/js/index.js
@@ -1,14 +1,43 @@
+const goToCreateUser = () => {
+    window.location.href = "create_user.html";
+}
+
+p_create_user.addEventListener('click', goToCreateUser);
+
+const message = p_message;
+const form_login = document.querySelector('#form_login');
+const login_first_name = document.querySelector('#login_first_name');
+const login_last_name = document.querySelector('#login_last_name');
+
+function lookForUser() {
+    fetch('https://testapi.io/api/4seven/resource/Users', {
+        method: 'get',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(obj => obj.json())
+        .then(userData => {
+            console.log(userData);
+            for (const user of userData.data) {
+                if (user.FirstName === login_first_name.value &&
+                    user.LastName === login_last_name.value) {
+                    message.innerHTML = 'Vartotojas rastas ' + user.Email;
+                }
+            }
+        })
+        .catch((err) => message.innerHTML = err);
+}
+
+const login_button = document.querySelector('#login_button');
+login_button.addEventListener('click', (e) => {
+    e.preventDefault(); // Breaks manual refresh after submit
+    lookForUser();
+})
 
 
 
-//form_login
-//form_login_submit
-
-
-//form_create_user
-//form_create_user_submit
 
 
 
-
-//message
