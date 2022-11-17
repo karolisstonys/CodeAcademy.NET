f6ef87a778222593115aa027a506dbd401ac442b
Karolis <4seven@gmail.com>
2022-11-17 19:08:26 +0200

    rest endpoints

---
+++ .NET/L02_Rest_Endpoints/L02_Rest_Endpoints/Controllers/FoodController.cs
@@ -0,0 +1,29 @@
+﻿using L02_Rest_Endpoints.Data;
+using L02_Rest_Endpoints.Models;
+using L02_Rest_Endpoints.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02_Rest_Endpoints.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        [HttpGet("allfood")]
+        public IEnumerable<FoodDTO> GetAllFood()
+        {
+            var allfood = FoodStore.foodList;
+            return allfood;
+        }
+
+        [HttpGet("foods/{id:int}")]
+        public FoodDTO? GetFoodById(int id)
+        {
+            return FoodStore.foodList
+                .FirstOrDefault(f => f.Id == id);
+        }
+
+
+    }
+}
---
+++ .NET/L02_Rest_Endpoints/L02_Rest_Endpoints/Models/Dto/FoodDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace L02_Rest_Endpoints.Models.Dto
+{
+    public class FoodDTO
+    {
+        private static int counter = 0;
+        public FoodDTO(int id, string name, string country, double weight)
+        {
+            //Id = counter;
+            //counter++;
+            Id = id;
+            Name = name;
+            Country = country;
+            Weight = weight;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public double Weight { get; set; }
+    }
+}
